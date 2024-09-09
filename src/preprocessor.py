70d755b57afd895c922ca04c059585d0444fc63e
Tiaan Viviers <tiaanviv@gmail.com>
2024-09-09 21:30:11 +0200

    all preprocessing

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,40 +1,49 @@
-from numpy import isnan
+from statistics import median, variance, mean
 
-def valid_ph(reading):
-    if isnan(reading):
-        return False
-    
-    if (reading < 2) or (reading > 12):
-        return False
-    
-    return True
-
-def valid_illuminance(reading):
-    if isnan(reading):
-        return False
-    
-    if (reading < 0) or (reading > 100_000):
-        return False
-    
-    return True
-    
-
-def valid_water_temp(reading):
-    if isnan(reading):
-        return False
-    
-    if (reading < 0) or (reading > 40):
-        return False
-    
-    return True
+def is_const_err(window_vals, limit):
+    if variance(window_vals) < limit:
+        return True
+    return False
 
 
-def valid_ec(reading):
-    if isnan(reading):
-        return False
-    
-    if (reading < 0) or (reading > 20):
-        return False
-    
-    return True
-    
+def med_filter(window_vals, med_window):
+    med_arr = window_vals[:med_window]
+    mid = med_window // 2
+
+    if (med_arr[mid] != median(med_arr)):
+        med_arr[mid] = median(med_arr)
+
+    window_vals = med_arr + window_vals[med_window:]
+    return window_vals
+
+
+def range_check(window_vals, UL, LL, all_vals):
+    if all_vals:
+        for i in range(len(window_vals)):
+            if (window_vals[i] > UL) or (window_vals[i] < LL):
+                temp = get_excluder_arr(window_vals, i)
+                window_vals[i] = mean(temp)
+    else:
+        if (window_vals[-1] > UL) or (window_vals[-1] < LL):
+            window_vals.pop()
+            window_vals.append(mean(window_vals))
+
+    return window_vals
+
+
+def get_excluder_arr(arr, index):
+    excluded = []
+    excluded += arr[:index]
+    excluded += arr[index+1:]
+    return excluded
+
+
+
+def main():
+    window = [10,9,10,100,13,14,10]
+    print(range_check(window, 75, -12, True))
+
+
+
+if __name__ == "__main__":
+    main()
